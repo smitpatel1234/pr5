48a6d2732b084ceee1fce1b4ab99befd1bcb4c24
smitpatel1234 <smitpatel53571@gmail.com>
2025-09-14 18:40:46 +0530

    second commit

--- fecher/fecher/wsgi.py
+++ fecher/fecher/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fecher.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fecher.fecher.settings')
 
 application = get_wsgi_application()
